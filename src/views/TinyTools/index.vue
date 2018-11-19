6c75729f090bd8884c7dc256eb43e155711b160e
cc <guoliwei1992@qq.com>
2018-11-19 16:13:23 +0800

    截取

--- src/views/TinyTools/index.vue
+++ src/views/TinyTools/index.vue
@@ -1,67 +1,95 @@
 <template>
     <div v-title="'小工具'">
-        <el-row>
-            <el-col :span="10" :offset="8">
+        <el-row class='row_t'>
+            <el-col :span="16" :offset="7">
                 <!-- <el-row>
                     <el-button type="primary" icon="el-icon-refresh">处理</el-button>
                     <el-button type="primary" icon="el-icon-edit">复制</el-button>
                     <el-button type="primary" icon="el-icon-delete">清空</el-button>
                 </el-row> -->
-                <el-checkbox :indeterminate="isIndeterminate" v-model="checkAll" @change="handleCheckAllChange">全选</el-checkbox>
-                <div style="margin: 15px 0;"></div>
-                <el-checkbox-group v-model="checkedCities" @change="handleCheckedCitiesChange">
-                    <el-checkbox v-for="city in cities" :label="city" :key="city">{{city}}</el-checkbox>
-                </el-checkbox-group>
+                <el-row>
+                    <el-radio v-model="radio" label="0" border @change="getLength">原长度</el-radio>
+                    <el-radio v-model="radio" label="1" border @change="getLength">身份证号</el-radio>
+                    <el-radio v-model="radio" label="2" border @change="getLength">残疾证号</el-radio>
+                    <el-radio v-model="radio" label="3" border @change="getLength">自定义长度</el-radio>
+                </el-row>
             </el-col>
         </el-row>
-        <el-row>
+        <el-row  v-if = disInput class='row_t'>
+            <el-col :span="10" :offset="7">
+                <el-row>
+                    <el-input v-model="input" placeholder="请输入内容"  clearable></el-input>
+                </el-row>
+            </el-col>
+        </el-row>
+        <el-row class='row_t'>
             <el-col :span="10" :offset="1">
-                <el-input id='words' type="textarea" :rows="9" placeholder="请输入内容" @change="getValue" >
+                <el-input id='words' type="textarea" :rows="9" placeholder="请输入内容,以空格分割" @change="getValue">
                 </el-input>
             </el-col>
             <el-col :span="10" :offset="1">
-                <el-input type="textarea" :rows="9" placeholder="请输入内容" v-model="textarea">
+                <el-input type="textarea" :rows="9" placeholder="请等待结果" v-model="textarea">
                 </el-input>
             </el-col>
         </el-row>
     </div>
 </template>
 <script>
-const typeOptions = ['身份证号', '残疾证号', '原长度'];
 export default {
-    name: "tools",
-    data() {
-        return {
-            textarea: '',
-            checkAll: false,
-            checkedCities: ['原长度'],
-            cities: typeOptions,
-            isIndeterminate: true
-        }
+  name: "tools",
+  data() {
+    return {
+      textarea: "",
+      radio: "0",
+      input: "",
+      disInput: false
+    };
+  },
+  methods: {
+    getValue(val) {
+      let v = [];
+      val
+        .trim()
+        .split(/\s/)
+        .forEach(e => {
+          e = e.split(/\s+/);
+          console.log(e);
+          let temp = "";
+          if (e.length > 1) {
+            temp = e[1];
+          } else {
+            temp = e[0];
+          }
+          console.log(this.radio);
+          if (this.radio == 1) {
+            temp = temp.substring(0,18);
+          } else if (this.radio == 2) {
+            temp = temp.substring(0,20);
+          } else if (this.radio == 3) {
+            console.log(temp.substring(0,this.input));
+            temp = temp.substring(0,this.input);
+          }
+          v.push("'" + temp + "'");
+        });
+      this.textarea = v.join(",");
     },
-    methods: {
-        getValue(val) {
-            let v = [];
-            val.trim().split(',').forEach(e => {
-                    e = e.split(/\s+/);
-                    if(e.length > 1){
-                        v.push("'" + e[1] + "'") ;
-                    }else{
-                        v.push("'" + e[0] + "'")
-                    }
-                });
-            this.textarea = v.join(',');
-        },
-        handleCheckAllChange(val) {
-        this.checkedCities = val ? typeOptions : [];
-        this.isIndeterminate = false;
-        },
-        handleCheckedCitiesChange(value) {
-            let checkedCount = value.length;
-            this.checkAll = checkedCount === this.cities.length;
-            this.isIndeterminate = checkedCount > 0 && checkedCount < this.cities.length;
-        }
-    
+    getLength(val) {
+      if (val === "3") {
+        this.disInput = true;
+      } else {
+        this.disInput = false;
+        this.input = "";
+      }
     }
-}
+  }
+};
 </script>
+<style  lang="less">
+ .el-row.row_t {
+    margin-bottom: 20px;
+    
+  }
+  /* .el-col {
+    border-radius: 4px;
+  } */
+</style>
